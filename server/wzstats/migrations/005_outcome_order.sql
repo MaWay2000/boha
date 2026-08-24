623d84818152c2bc46d0df9faf7a1896ccd63da2
MaWay2000 <mantasmatuseviciuz@gmail.com>
2026-08-24 03:48:13 +0300

    updated repository privacy and removed public backend source

--- server/wzstats/migrations/005_outcome_order.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE match_outcome_facts
-    ADD COLUMN IF NOT EXISTS legacy_order INT UNSIGNED NOT NULL DEFAULT 0 AFTER result_source;
--- server/wzstats/migrations/008_replay_stats_source_default.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE match_players
-    MODIFY stats_source VARCHAR(32) NOT NULL DEFAULT 'replay';
